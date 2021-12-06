a4141bfac027fffc755cbe487d5e2ed1f7d8bde0
Juansantoss07 <juansantosscunha@gmail.com>
2021-12-06 13:57:11 -0300

    Update aula 19 CRUD1

---
+++ app/model/conexao.php
@@ -0,0 +1,19 @@
+<?php 
+
+class Conexao {
+
+    private static $instance; //-> Esse método vai verificar se já existe uma instância da conexão, se existir ele irá retornar a instância
+
+    public static function getConn(){
+
+        if(!isset(self::$instance)): //Nessa linha estamos usando o simbolo de negação (!) e em seguida o isset. Nesse caso podemos ler da seguinte forma: "Se não existir" e em seguida colocamos self:: por ser estático e o nome da váriavel. Ai fica: "Se não existir uma instância..."
+            self::$instance = new PDO('mysql:host=localhost; dbname=pdo; charset=utf8','root','');
+            //Se não existir a instância então ele retorna uma nova. Nessa linha criamos uma conexão.
+        else: 
+            return self::$instance;
+            //Se existir ele vai retornar a instância.
+        endif;
+    }
+
+    
+}
---
+++ app/model/produto.php
@@ -0,0 +1,24 @@
+<?php
+
+class Produto{
+
+    private $nome, $descricao;
+
+    public function getNome(){
+        return $this->nome;
+    }
+
+    public function setNome($nome){
+        $this->nome = $nome;
+    }
+
+    public function getDescricao(){
+        return $this->descricao;
+    }
+
+    public function setDescricao($descricao){
+        $this->descricao = $descricao;
+    }
+}
+
+?>
