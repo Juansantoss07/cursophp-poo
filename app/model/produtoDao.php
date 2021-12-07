c49ea0b094736b11cff18ce486e855ae72b24907
Juansantoss07 <juansantosscunha@gmail.com>
2021-12-07 01:10:41 -0300

    Update aula20-pt2

--- app/model/produtoDao.php
+++ app/model/produtoDao.php
@@ -6,6 +6,13 @@ class ProdutoDao{
 
     public function create(Produto $p) {
 
+        $sql = 'INSERT INTO produtos (nome, descricao) VALUES (?,?)';
+
+        $stmt = Conexao::getConn()->prepare($sql);
+        $stmt->bindValue(1, $p->getNome());
+        $stmt->bindValue(2, $p->getDescricao());
+
+        $stmt->execute();
     }
 
     public function read(){
--- aula20.php
+++ aula20.php
@@ -2,4 +2,10 @@
 
 require_once 'vendor/autoload.php';
 
+$produto = new \app\model\Produto();
+$produto->setNome('Notebook DELL');
+$produto->setDescricao('i5, 4Gb');
+
+$produtoDao = new \app\model\ProdutoDao();
+$produtoDao->create($produto);
 ?>
